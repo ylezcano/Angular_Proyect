6171ce014879161c805cdaa6bf8dc3a6beb76ace
Yonny Lezcano <lezcano2535@hotmail.es>
2020-08-11 23:23:14 -0500

    primer commit del proyecto angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarComponent } from './Persona/listar/listar.component';
+import { EditComponent } from './Persona/edit/edit.component';
+import { AddComponent } from './Persona/add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarComponent,
+    EditComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
